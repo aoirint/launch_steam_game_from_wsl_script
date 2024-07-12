6c8badc06332efc24a8cb394e75d2a3dbab69a2c
aoirint <aoirint@gmail.com>
2024-07-12 22:17:03 +0900

    first (created at 2022-08-04)

---
+++ direct.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+APP="/mnt/c/Program Files (x86)/Steam/steamapps/common/Among Us/Among Us.exe"
+"$APP" -screen-fullscreen 0 -window-mode borderless -screen-width 640 -screen-height 360 -popupwindow
+
---
+++ viasteam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+STEAM="/mnt/c/Program Files (x86)/Steam/steam.exe"
+"$STEAM" -applaunch 945360 -screen-fullscreen 0 -window-mode borderless -screen-width 640 -screen-height 360 -popupwindow
+
